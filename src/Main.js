0437fcd92c0aa42205d55e0b54abcece5e8be78b
Severuna <oxxxyfomenkova@gmail.com>
2023-06-02 00:52:29 +0300

    test

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Main.css';
 
 const Main = () => {
+    const [films, setFilms] = useState([]);
+    const getData = async () => {
+        await fetch("https://swapi.dev/api/films/")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setFilms(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }
+    getData();
     return (
         <div>
             <h1>main</h1>
